a5d20f145fc8f4c251c2ae209b29db5ce0f79d1a
Cooper-S123 <cooper.sunderman@student.scots.college>
2025-07-01 14:07:22 +1000

    Made it so every time you click the button it doesn't just add content

--- src/page-exports.js
+++ src/page-exports.js
@@ -6,28 +6,33 @@ const projects = [
             desc: "This is a default todo, showing the format.",
             dueDate: "N/A",
             priority: "low",
-            note: "learn how to use this tool"
+            note: "learn how to use this tool",
+            display: false
         },
         todo1: {
-            title: "default TODO",
+            title: "default hello",
             desc: "This is a default todo, showing the format.",
-            dueDate: "N/A",
+            dueDate: "1999",
             priority: "low",
-            note: "learn how to use this tool"
+            note: "learn how to use this tool",
+            display: false
         }
     }
    ];
 
+let iterations = 0;
 function load_project(project) {
    const main = document.querySelector("main");
+   const removal_wrapper = document.createElement("div");
    const project_head = document.createElement("h2");
    const todo_head = document.createElement("h3");
-
+   
+   removal_wrapper.setAttribute("id", "removal-wrapper");
    project_head.textContent = `Project: ${project.title}`;
    todo_head.textContent = "Todos: ";
 
-   main.appendChild(project_head);
-   main.appendChild(todo_head);
+   removal_wrapper.appendChild(project_head);
+   removal_wrapper.appendChild(todo_head);
    for (let item in project) {
       if (item === "title") {
         continue;
@@ -36,33 +41,58 @@ function load_project(project) {
       const todoBtn = document.createElement("button");
 
       main_wrapper.classList.add("main-wrapper");
-      todoBtn.textContent = project.todo.title;
+      todoBtn.textContent = project[item].title;
       todoBtn.addEventListener("click", () => {
-        const info_wrapper = document.createElement("div");
-        const info_head = document.createElement("h4");
-        const info_desc = document.createElement("p");
-        const info_dueDate = document.createElement("p");
-        const info_priority = document.createElement("p");
-        const info_note = document.createElement("p");
+        if (project[item].display === false) {
+            project[item].display = true;
+         }
+         else if (project[item].display === true) {
+           project[item].display = false;
+         }
+        
+        if (project[item].display === true) {
+            const info_wrapper = document.createElement("div");
+            const info_head = document.createElement("h4");
+            const info_desc = document.createElement("p");
+            const info_dueDate = document.createElement("p");
+            const info_priority = document.createElement("p");
+            const info_note = document.createElement("p");
 
-        info_note.textContent = `Notes: ${project.todo.note}`;
-        info_priority.textContent = `Priority level: ${project.todo.priority}`;
-        info_dueDate.textContent = `Due date: ${project.todo.dueDate}`;
-        info_desc.textContent = `Description: ${project.todo.desc}`;
-        info_head.textContent = project.todo.title;
-        info_wrapper.classList.add("info-wrapper");
+            info_note.textContent = `Notes: ${project[item].note}`;
+            info_priority.textContent = `Priority level: ${project[item].priority}`;
+            info_dueDate.textContent = `Due date: ${project[item].dueDate}`;
+            info_desc.textContent = `Description: ${project[item].desc}`;
+            info_head.textContent = project[item].title;
+            info_wrapper.classList.add("info-wrapper");
+            info_wrapper.setAttribute("id", "info-wrapper")
 
-        info_wrapper.appendChild(info_head);
-        info_wrapper.appendChild(info_desc);
-        info_wrapper.appendChild(info_dueDate);
-        info_wrapper.appendChild(info_priority);
-        info_wrapper.appendChild(info_note)
-        main_wrapper.appendChild(info_wrapper);
+            info_wrapper.appendChild(info_head);
+            info_wrapper.appendChild(info_desc);
+            info_wrapper.appendChild(info_dueDate);
+            info_wrapper.appendChild(info_priority);
+            info_wrapper.appendChild(info_note)
+            main_wrapper.appendChild(info_wrapper);
+        }
+        else {
+            const info_remove = document.getElementById("info-wrapper");
+            info_remove.remove();
+        }
       });
 
       main_wrapper.appendChild(todoBtn);
-      main.appendChild(main_wrapper);
+      removal_wrapper.appendChild(main_wrapper);
+      main.appendChild(removal_wrapper);
    }
+   iterations++;
+}
+
+function reset(project) {
+    for (let item in project) {
+        if (item === "title") {
+          continue;
+        }
+       project[item].display = false;
+    }
 }
 
 export function projects_bar() {
@@ -72,6 +102,11 @@ export function projects_bar() {
 
     project_button.textContent = projects[project].title;
     project_button.addEventListener("click", () => {
+        if (iterations > 0) {
+            const removal_wrapper = document.getElementById("removal-wrapper");
+            removal_wrapper.remove();
+            reset(projects[project]);
+        }
         load_project(projects[project]);
     });
 
