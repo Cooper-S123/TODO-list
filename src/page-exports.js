e1f3a6fd8785229cb7650077434e97de5aecebfa
Cooper-S123 <cooper.sunderman@student.scots.college>
2025-07-07 18:36:57 +1000

    Added the ability to remove projects and todos and made the data save to local storage

--- src/page-exports.js
+++ src/page-exports.js
@@ -1,4 +1,5 @@
-const projects = [
+let projects;
+const default_projects = [
     [
          "default",
          {
@@ -19,17 +20,37 @@ const projects = [
         }
     ]
    ];
-
+let currentProject;
 let iterations = 0;
+let recentlyRemoved = false;
+let forIterations = 0;
 function load_project(project) {
+   currentProject = project[0];
+   preScreen();
    const main = document.querySelector("main");
    const removal_wrapper = document.createElement("div");
    const project_head = document.createElement("h2");
    const todo_head = document.createElement("h3");
+   const deleteBtn = document.createElement("button");
    
+   deleteBtn.textContent = "delete project";
+   deleteBtn.classList.add("delete-project");
    removal_wrapper.setAttribute("id", "removal-wrapper");
    project_head.textContent = `Project: ${project[0]}`;
    todo_head.textContent = "Todos: ";
+
+   deleteBtn.addEventListener("click", () => {
+     let projectIndex = projects.indexOf(project);
+     projects.splice(projectIndex, 1);
+     const navBtn = document.querySelectorAll("#projectBtn");
+     navBtn.forEach((button) => {
+           button.remove();
+     }); 
+     displayProjects();
+     removal_wrapper.remove();
+     recentlyRemoved = true;
+     saveProjects();
+   });
 
    removal_wrapper.appendChild(project_head);
    removal_wrapper.appendChild(todo_head);
@@ -39,7 +60,14 @@ function load_project(project) {
       }
       const main_wrapper = document.createElement("div");
       const todoBtn = document.createElement("button");
+      const removeBtn = document.createElement("button");
+      const buttons = document.createElement("div");
+      const buttonCenter = document.createElement("div");
 
+      buttonCenter.classList.add("center-removeBtn");
+      removeBtn.textContent = "X";
+      removeBtn.classList.add("remove-todo");
+      buttons.classList.add("todo-buttons");
       main_wrapper.classList.add("main-wrapper");
       todoBtn.textContent = project[item].title;
       todoBtn.addEventListener("click", () => {
@@ -79,11 +107,59 @@ function load_project(project) {
         }
       });
 
-      main_wrapper.appendChild(todoBtn);
+      removeBtn.addEventListener("click", () => {
+           project.splice(item, 1);
+           main_wrapper.remove();
+           saveProjects();
+      });
+
+      buttonCenter.appendChild(removeBtn);
+      buttons.appendChild(todoBtn);
+      buttons.appendChild(buttonCenter);
+      main_wrapper.appendChild(buttons);
       removal_wrapper.appendChild(main_wrapper);
-      main.appendChild(removal_wrapper);
    }
+   removal_wrapper.appendChild(deleteBtn);
+   main.appendChild(removal_wrapper);
    iterations++;
+}
+
+function storageAvailable(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = "__storage_test__";
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return false;
+    }
+}
+
+function getProjects() {
+    if (localStorage.getItem("projects") === undefined || localStorage.getItem("projects") === null) {
+        projects = default_projects;
+    }
+    else {
+        projects = JSON.parse(localStorage.getItem("projects"));
+    }
+    
+}
+
+function saveProjects() {
+    localStorage.setItem("projects", JSON.stringify(projects));
+    addTodoFunction();
+}
+
+function load_storage() {
+    if (storageAvailable("localStorage")) {
+        getProjects();
+    }
+    else {
+        projects = default_projects;
+    }
+    addTodoFunction();
 }
 
 function reset(project) {
@@ -125,7 +201,7 @@ function errorPage() {
    });
 }
 
-function dialog(currentProject, project) {
+function addTodo(project) {
     const showDialog = document.querySelector("#showDialog");
     const dialog = document.querySelector("dialog");
     const confirmBtn = document.querySelector("#confirmBtn");
@@ -136,6 +212,9 @@ function dialog(currentProject, project) {
 
     confirmBtn.addEventListener("click", (event) => {
       try {
+        if (project[0] !== currentProject && currentProject !== undefined) {
+           return;
+        }
         const newTitle = document.querySelector("#title");
         const newDesc = document.querySelector("#desc");
         const newDate = document.querySelector("#date");
@@ -143,7 +222,7 @@ function dialog(currentProject, project) {
         const newNotes = document.querySelector("#note");
         
         let newTodo = new Todo(newTitle.value, newDesc.value, newDate.value, newPriority.value, newNotes.value);
-        projects[currentProject].push(newTodo);
+        project.push(newTodo);
 
         event.preventDefault();
         dialog.close();
@@ -152,28 +231,97 @@ function dialog(currentProject, project) {
         removal_wrapper.remove();
         load_project(project);
       } catch (error) {
-         projects[currentProject].splice((projects[currentProject].length - 1), 1);
+         if (project.length > 0) {
+           project.splice((project.length - 1), 1);
+         }
+         else {
+            project.spice(project.length, 1);
+         }
          errorPage();
       }
+      saveProjects();
     });
 }
 
-export function projects_bar() {
-   const nav = document.querySelector("nav");
-   for (let project in projects) {
-    const project_button = document.createElement("button");
-
-    project_button.textContent = projects[project][0];
-    project_button.addEventListener("click", () => {
-        if (iterations > 0) {
-            const removal_wrapper = document.getElementById("removal-wrapper");
-            removal_wrapper.remove();
-            reset(projects[project]);
-        }
-        load_project(projects[project]);
+function addProject() {
+    const dialog = document.querySelector("#addProject");
+    const showBtn = document.querySelector("#newProject");
+    const confirmBtn = document.querySelector("#submitBtn");
+     
+    showBtn.addEventListener("click", () => {
+       dialog.showModal();
     });
-   
-    nav.appendChild(project_button);
-    dialog(project, projects[project]);
-   }
+    
+    confirmBtn.addEventListener("click", () => {
+       const newName = document.querySelector("#name");
+
+       let newProject = [newName.value];
+       projects.push(newProject);
+
+       const removeBtns = document.querySelectorAll("#projectBtn");
+
+       removeBtns.forEach((button) => {
+          button.remove();
+       });
+       displayProjects();
+       saveProjects();
+    });
+}
+
+function preScreen() {
+    if (currentProject === undefined) {
+        const main = document.querySelector("main");
+        const text = document.createElement("h4");
+
+        text.id = "preText";
+        text.textContent = "Select a project to see/add Todos!";
+
+        main.appendChild(text);
+    }
+    else {
+       const removeText = document.querySelector("#preText");
+       if (removeText !== null) {
+         removeText.remove();
+       }
+    }
+}
+
+function addTodoFunction() {
+    for (let project in projects) {
+        if (project <= forIterations - 1) {
+           continue;
+        }
+        addTodo(projects[project]);
+        forIterations++;
+    }
+}
+
+function displayProjects() {
+    const nav = document.querySelector("nav");
+    for (let project in projects) {
+        const project_button = document.createElement("button");
+
+        project_button.id = "projectBtn";
+        project_button.textContent = projects[project][0];
+        project_button.addEventListener("click", () => {
+            if (iterations > 0 && !recentlyRemoved) {
+                const removal_wrapper = document.getElementById("removal-wrapper");
+                removal_wrapper.remove();
+                reset(projects[project]);
+            }
+            else if (recentlyRemoved) {
+                recentlyRemoved = false;
+            }
+            load_project(projects[project]);
+        });
+       
+        nav.appendChild(project_button);
+       }
+}
+
+export function projects_bar() {
+   load_storage();
+   preScreen();
+   addProject();
+   displayProjects();
 }
